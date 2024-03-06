72e02060a668fd51f0d49725b344cac94d97db6a
= <=>
2024-03-05 20:16:13 -0800

    Updated test code

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include "interfaces/buffer.h"
+#include <unistd.h>
 using namespace std;
-
-
 
 int main(){
 	Framebuffer<128, 64> framebuffers;
 
-	DisplayDriverTemplated driver(framebuffers);
-	driver.drawSquare(0, 5, {.r=255, .g=255, .b=255});
-	
-	driver.display();
+	DisplayDriver driver(framebuffers);
+	/* driver.draw_pixel(0, 5, {.r=255, .g=255, .b=255}); */
+	/* driver.draw_line(5, 5, 15, 10, {.r=255, .g=255, .b=255}); */
+
+	for(int i = 0; i < 10; i++){
+		for(int j = 0; j < 20; j++){
+			driver.refresh();
+			driver.draw_square({i, j}, 10, 10, {.r=0, .g=0, .b=255});
+			sleep(1);
+		}
+	}
+	/* driver.draw_square({.x = 8, .y=6}, 10, 10, {.r=255, .g=0, .b=0}); */
 
 	return 0;
 }
